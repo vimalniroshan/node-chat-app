6d28502bba19f33508642306c278bb1c9edf5f7c
vimalniroshan <vimalniroshan@users.noreply.github.com>
2017-01-22 19:27:39 -0800

    Add connect disconnet event handled

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 const path = require('path');
 
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(port, () => {
   console.log(`Started server on port ${port}`);
 })
